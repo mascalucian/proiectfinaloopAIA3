a2c3a70ad21383e086b49ece2106830e526cdb2d
Bumburnyak <91269525+Bumburnyak@users.noreply.github.com>
2021-10-27 19:19:34 +0300

    Update app.java

--- src/ProiectFinal/app.java
+++ src/ProiectFinal/app.java
@@ -40,26 +40,46 @@ public class app {
                 boolean isNumeNumeric=isNumeric(textField1.getText());
                 boolean isNreleviNumeric=isNumeric(textField4.getText());
 
-                if(isNumeNumeric==false) {mesaj.setText("Eroare: Numarul clasei nu poate contine litere.");}
-                else if (isNreleviNumeric==false) {mesaj.setText("Eroare: Numarul de elevi nu poate contine litere.");}
+                if(isNumeNumeric==false) {mesaj.setText("Eroare: Numarul clasei nu poate contine litere. SAU campul este gol");}
+                else if (isNreleviNumeric==false) {mesaj.setText("Eroare: Numarul de elevi nu poate contine litere. SAU campul este gol");}
                 else {
+
+                    boolean b = true;
+                    if(textField2.getText().isEmpty()){
+                        b = false;
+                        JOptionPane.showConfirmDialog(null, "Nu ai completat campul 'litera'",
+                                "Atentie",
+                                JOptionPane.DEFAULT_OPTION,
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+
+                    else if(!(textField2.getText().equals("A") || textField2.getText().equals("B") || textField2.getText().equals("C") || textField2.getText().equals("D"))){
+                        b = false;
+                        JOptionPane.showConfirmDialog(null, "Litera nu este A sau B sau C sau D",
+                                "Atentie",
+                                JOptionPane.DEFAULT_OPTION,
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+
+                    if(b){
                     Integer nr = Integer.parseInt(textField4.getText());
                     String nrString = nr.toString();
                     clasee.add(new Clasa(textField1.getText(), textField2.getText(), comboBox1.getSelectedItem().toString(), nrString));
                     textField1.setText("");
                     textField2.setText("");
-                    comboBox1.toString();
                     textField4.setText("");
                     String clasa = clasee.get(clasee.size() - 1).getClasa();
                     String litera = clasee.get(clasee.size() - 1).getLitera();
                     String specializare = clasee.get(clasee.size() - 1).getSpecializare();
                     String nrelevi = clasee.get(clasee.size() - 1).getNrelevi();
+
                     mesaj.setText("Clasa " + clasa + litera + " a fost adaugata cu succes.");
                     System.out.println(clasa + litera);
                     StringBuffer sb = new StringBuffer();
                     sb.append(textArea1.getText());
                     sb.append("\r\n" + clasee.size() + ". " + clasa + litera + " - Specializarea: " + specializare + ", avand un numar de " + nrelevi + " elevi");
                     textArea1.setText(String.valueOf(sb));
+                    }
                 }
             }
         });
